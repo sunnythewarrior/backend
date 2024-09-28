e268c8982961c8a13406a2276e3b7bade171efe6
sunnythewarrior <sunnyranalkar18@gmail.com>
2024-09-28 16:54:01 +0530

    msg chng

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
-  res.json({ message: "Hello Rian" });
+  res.json({ message: "Hello" });
 });
 app.listen(3000);
